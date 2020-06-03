5e54c2e65d35514fd9692600803d7b40c8211673
richmondu <richmond.umagat@yahoo.com>
2020-06-03 13:55:10 +0800

    Update braintree_manager.py

--- braintree_manager.py
+++ braintree_manager.py
@@ -16,22 +16,32 @@ CORS(app)
 def index():
 	return json.dumps({'status': 'OK'})
 
+# Request:
+#   GET /client_token
+#   headers: {'Content-Type': 'application/json'}
+# Response:
+#   {'status': 'OK', 'token': string}
 @app.route('/client_token', methods=['GET'])
 def get_client_token():
 	token = braintree.generate_client()
+	print("\nclient_token:\n{}\n".format(token))
 	return json.dumps({'status': 'OK', 'token': token})
 
-def _get_plan_amount(plan_name):
-	plans = [
-		{"name": "Basic10",      "amount": "10.00"},
-		{"name": "Pro30",        "amount": "30.00"},
-		{"name": "Enterprise50", "amount": "50.00"}
-	]
-	for plan in plans:
-		if plan["name"] == plan_name:
-			return plan["name"], float(plan["amount"])
-	return None, float(0)
-
+# Request:
+#   POST /nonce
+#   headers: {'Content-Type': 'application/json'}
+#   data: {
+#     'plan':        string, // Subscription plan type Basic10, Pro30, Enterprise50
+#     'nonce':       string, // Nonce value returned by Braintree to Frontend
+#     'type':        string, // Payment method type - Paypal, CreditCard, Venmo, ApplePay, GooglePay, SamsungPay
+#     'details': {
+#       'email':     string,
+#       'firstName': string,
+#       'lastName':  string,
+#     }
+#   }
+# Response:
+#   {'status': 'OK', 'token': string}
 @app.route('/nonce', methods=['POST'])
 def process_transaction():
 	data = flask.request.get_json()
@@ -51,6 +61,17 @@ def process_transaction():
 
 	result, msg = braintree.process_transaction_recurring(PLAN, PLAN_AMOUNT, TYPE, NONCE, EMAIL, FIRST_NAME, LAST_NAME)
 	return json.dumps({'status': 'OK', 'msg': msg})
+
+def _get_plan_amount(plan_name):
+	plans = [
+		{"name": "Basic10",      "amount": "10.00"},
+		{"name": "Pro30",        "amount": "30.00"},
+		{"name": "Enterprise50", "amount": "50.00"}
+	]
+	for plan in plans:
+		if plan["name"] == plan_name:
+			return plan["name"], float(plan["amount"])
+	return None, float(0)
 
 def initialize():
 	global braintree
